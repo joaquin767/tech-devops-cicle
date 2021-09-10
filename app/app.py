6eb8aaeadfeb9cac87baea19bef06405c47828f3
Joaquin Veliz <joaquin.gveliz@gmail.com>
2021-09-10 16:33:36 -0300

    lint and added test jobs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from models import Client
 
 app = Flask(__name__)
 
@@ -9,55 +9,64 @@ app.config.from_envvar('PSQL_SETTINGS')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Client
 
-#Index
+# Index
+
 @app.route("/")
 def index():
     return "This is the app index"
 
-#Add client and money
+# Add client and money
+
+
 @app.route("/add")
 def add_client():
-    name=request.args.get('name')
-    money=request.args.get('money')
+    name = request.args.get('name')
+    money = request.args.get('money')
     try:
-        client=Client(
+        client = Client(
             name=name,
             money=money
         )
         db.session.add(client)
         db.session.commit()
         return "Client added with id={}".format(client.id)
-    except Exception as e:
-	    return(str(e))
+    except Exception as error:
+        return str(error)
 
-#Get all clients
+# Get all clients
+
+
 @app.route("/getall")
 def get_all():
     try:
-        clients=Client.query.all()
-        return  jsonify([e.serialize() for e in clients])
-    except Exception as e:
-	    return(str(e))
+        clients = Client.query.all()
+        return jsonify([e.serialize() for e in clients])
+    except Exception as error:
+        return str(error)
 
-#Get client by ID
+# Get client by ID
+
+
 @app.route("/get/<id_>")
 def get_by_id(id_):
     try:
-        client=Client.query.filter_by(id=id_).first()
+        client = Client.query.filter_by(id=id_).first()
         return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
+    except Exception as error:
+        return str(error)
 
-#Get client by Name
+# Get client by Name
+
+
 @app.route("/getn/<name_>")
 def get_by_name(name_):
     try:
-        client=Client.query.filter_by(name=name_).first()
+        client = Client.query.filter_by(name=name_).first()
         return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
+    except Exception as error:
+        return str(error)
+
 
 if __name__ == '__main__':
     app.run()
