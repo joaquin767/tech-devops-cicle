27bae12068f06d63eb58b0cd1d408d8917086190
Joaquin Veliz <joaquin.gveliz@gmail.com>
2021-09-10 07:28:59 -0300

    ingress added

--- app/app.py
+++ app/app.py
@@ -1,63 +1,70 @@
-import os
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app = Flask(__name__)
 
-app.config.from_envvar('APP_SETTINGS')
-app.config.from_envvar('PSQL_SETTINGS')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from models import Client
-
-#Index
 @app.route("/")
-def index():
-    return "This is the app index"
+def hello_world():
+    return "<p>Hello, World!</p>"
+# import os
+# from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
 
-#Add client and money
-@app.route("/add")
-def add_client():
-    name=request.args.get('name')
-    money=request.args.get('money')
-    try:
-        client=Client(
-            name=name,
-            money=money
-        )
-        db.session.add(client)
-        db.session.commit()
-        return "Client added with id={}".format(client.id)
-    except Exception as e:
-	    return(str(e))
+# app = Flask(__name__)
 
-#Get all clients
-@app.route("/getall")
-def get_all():
-    try:
-        clients=Client.query.all()
-        return  jsonify([e.serialize() for e in clients])
-    except Exception as e:
-	    return(str(e))
+# app.config.from_envvar('APP_SETTINGS')
+# app.config.from_envvar('PSQL_SETTINGS')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-#Get client by ID
-@app.route("/get/<id_>")
-def get_by_id(id_):
-    try:
-        client=Client.query.filter_by(id=id_).first()
-        return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
+# from models import Client
 
-#Get client by Name
-@app.route("/getn/<name_>")
-def get_by_name(name_):
-    try:
-        client=Client.query.filter_by(name=name_).first()
-        return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
+# #Index
+# @app.route("/")
+# def index():
+#     return "This is the app index"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# #Add client and money
+# @app.route("/add")
+# def add_client():
+#     name=request.args.get('name')
+#     money=request.args.get('money')
+#     try:
+#         client=Client(
+#             name=name,
+#             money=money
+#         )
+#         db.session.add(client)
+#         db.session.commit()
+#         return "Client added with id={}".format(client.id)
+#     except Exception as e:
+# 	    return(str(e))
+
+# #Get all clients
+# @app.route("/getall")
+# def get_all():
+#     try:
+#         clients=Client.query.all()
+#         return  jsonify([e.serialize() for e in clients])
+#     except Exception as e:
+# 	    return(str(e))
+
+# #Get client by ID
+# @app.route("/get/<id_>")
+# def get_by_id(id_):
+#     try:
+#         client=Client.query.filter_by(id=id_).first()
+#         return jsonify(client.serialize())
+#     except Exception as e:
+# 	    return(str(e))
+
+# #Get client by Name
+# @app.route("/getn/<name_>")
+# def get_by_name(name_):
+#     try:
+#         client=Client.query.filter_by(name=name_).first()
+#         return jsonify(client.serialize())
+#     except Exception as e:
+# 	    return(str(e))
+
+# if __name__ == '__main__':
+#     app.run()
